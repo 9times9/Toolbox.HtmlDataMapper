8e545818bf6386a2747d27f8b0f2d2148edcf662
Grzegorz Pawlik <gpawlik@nexway.com>
2013-09-11 17:21:29 +0200

    mapper works on inputs

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -3,3 +3,31 @@ describe("Some basic stuff", function(){
         expect(true).toBeTruthy();
     })
 })
+
+describe("Input tests", function(){
+    var data = {dataIndex: "123"},
+        mapper = new HtmlDataMapper()
+    ;
+    it("should set input value", function() {
+        var mappingData = {
+                dataIndex: document.getElementById('item_with_value_property')
+            }
+            ;
+
+        mapper.map(data, mappingData);
+
+        expect(document.getElementById('item_with_value_property').value).toBe("123")
+
+    })
+
+    it("should ignore when there's no mapping for data", function(){
+        var mappingData = {
+            notExistingIndex: document.getElementById('item_with_value_property')
+        }
+
+        expect(function(){
+            mapper.map(data, mappingData)
+        }).not.toThrow();
+
+    })
+})
